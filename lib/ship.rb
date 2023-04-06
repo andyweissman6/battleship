a98c3b483295799ec140019bc594aadba3786f9c
Andy Weissman <andyweissman6@gmail.com>
2023-04-06 17:37:34 -0600

    added sunk? and hit methods

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,8 +1,24 @@
 class Ship
   attr_reader :name,
-              :length
+              :length,
+              :health
+
   def initialize(name, length)
     @name = name
     @length = length
+    @health = 3
   end
+  
+  def sunk?
+    health < 1
+  end
+  
+  def hit
+    @health -= 1
+  end
+  
 end
+
+
+# require 'pry'; binding.pry
+# require 'pry'; binding.pry
