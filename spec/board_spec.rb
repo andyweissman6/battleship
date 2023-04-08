85200993f7e6ba78ca1a2c89499010903ee2a896
Andy Weissman <andyweissman6@gmail.com>
2023-04-08 15:03:51 -0600

    add tests for 'valid_coordinate?'

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -14,25 +14,29 @@ RSpec.describe Board do
       expect(board.cells).to be_a(Hash)
     end
   end
+
+  describe "#valid_coordinate?" do
+    it "checks for valid coordinates" do
+      expect(board.valid_coordinate?("A1")).to eq(true)
+      expect(board.valid_coordinate?("D4")).to eq(true)
+      expect(board.valid_coordinate?("A5")).to eq(false)
+      expect(board.valid_coordinate?("E1")).to eq(false)
+      expect(board.valid_coordinate?("A22")).to eq(false)
+    end
+  end
 end
 
-# pry(main)> board.cells
-# # =>
-# {
-#  "A1" => #<Cell:0x00007ff0728a3f58...>,
-#  "A2" => #<Cell:0x00007ff0728a3ee0...>,
-#  "A3" => #<Cell:0x00007ff0728a3e68...>,
-#  "A4" => #<Cell:0x00007ff0728a3df0...>,
-#  "B1" => #<Cell:0x00007ff0728a3d78...>,
-#  "B2" => #<Cell:0x00007ff0728a3d00...>,
-#  "B3" => #<Cell:0x00007ff0728a3c88...>,
-#  "B4" => #<Cell:0x00007ff0728a3c10...>,
-#  "C1" => #<Cell:0x00007ff0728a3b98...>,
-#  "C2" => #<Cell:0x00007ff0728a3b20...>,
-#  "C3" => #<Cell:0x00007ff0728a3aa8...>,
-#  "C4" => #<Cell:0x00007ff0728a3a30...>,
-#  "D1" => #<Cell:0x00007ff0728a39b8...>,
-#  "D2" => #<Cell:0x00007ff0728a3940...>,
-#  "D3" => #<Cell:0x00007ff0728a38c8...>,
-#  "D4" => #<Cell:0x00007ff0728a3850...>
-# }
+# pry(main)> board.valid_coordinate?("A1")
+# # => true
+
+# pry(main)> board.valid_coordinate?("D4")
+# # => true
+
+# pry(main)> board.valid_coordinate?("A5")
+# # => false
+
+# pry(main)> board.valid_coordinate?("E1")
+# # => false
+
+# pry(main)> board.valid_coordinate?("A22")
+# # => false
