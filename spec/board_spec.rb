5dc8a851d0aecf6ab5dc89f27309fc860dd5c212
Andy Weissman <andyweissman6@gmail.com>
2023-04-08 15:20:56 -0600

    add tests for 'valid_placement?'

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe Board do
     end
   end
 
-  describe "#valid_coordinate?" do
+  describe "#valid_coordinate?/1" do
     it "checks for valid coordinates" do
       expect(board.valid_coordinate?("A1")).to eq(true)
       expect(board.valid_coordinate?("D4")).to eq(true)
@@ -24,19 +24,47 @@ RSpec.describe Board do
       expect(board.valid_coordinate?("A22")).to eq(false)
     end
   end
+
+  describe "#valid_placement?/2" do
+    let(:cruiser) {Ship.new("Cruiser", 3)}
+    let(:submarine) {Ship.new("Submarine", 2)}
+
+    it "checks if number of coordinates in array is the same as the length of the cruiser" do
+      expect(board.valid_placement?(cruiser, ["A1", "A2"])).to eq(false)
+      expect(board.valid_placement?(cruiser, ["A1", "A2", "A3"])).to eq(true)
+    end
+
+    it "checks if number of coordinates in array is the same as the length of the submarine" do
+      expect(board.valid_placement?(submarine, ["A2", "A3", "A4"])).to eq(false)
+      expect(board.valid_placement?(submarine, ["A2", "A3"])).to eq(true)
+    end
+  end
 end
 
-# pry(main)> board.valid_coordinate?("A1")
+# Next, make sure the coordinates are consecutive:
+
+# pry(main)> board.valid_placement?(cruiser, ["A1", "A2", "A4"])
+# # => false
+
+# pry(main)> board.valid_placement?(submarine, ["A1", "C1"])
+# # => false
+
+# pry(main)> board.valid_placement?(cruiser, ["A3", "A2", "A1"])
+# # => false
+
+# pry(main)> board.valid_placement?(submarine, ["C1", "B1"])
+# # => false
+# Finally, coordinates can’t be diagonal:
+
+# pry(main)> board.valid_placement?(cruiser, ["A1", "B2", "C3"])
+# # => false
+
+# pry(main)> board.valid_placement?(submarine, ["C2", "D3"])
+# # => false
+# If all the previous checks pass then the placement should be valid:
+
+# pry(main)> board.valid_placement?(submarine, ["A1", "A2"])
 # # => true
 
-# pry(main)> board.valid_coordinate?("D4")
+# pry(main)> board.valid_placement?(cruiser, ["B1", "C1", "D1"])
 # # => true
-
-# pry(main)> board.valid_coordinate?("A5")
-# # => false
-
-# pry(main)> board.valid_coordinate?("E1")
-# # => false
-
-# pry(main)> board.valid_coordinate?("A22")
-# # => false
